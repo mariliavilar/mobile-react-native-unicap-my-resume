bbafe52d33280f6bbe78f164d1881e75c88a9392
Marilia Vilar <mariliavilarpr@gmail.com>
2021-12-12 22:46:10 -0300

    feature: ajustes na bottom material navigation

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-import { HomeScreen } from './screens/HomeScreen'
-import { AcademicScreen } from './screens/AcademicScreen'
-import { ProfessionalScreen } from './screens/ProfessionalScreen'
-import { SkillsScreen } from './screens/SkillsScreen'
-import { ExtraActivitiesScreen } from './screens/ExtraActivitiesScreen'
-
+import { MyTabMaterialNavigation } from './components/MyTabMaterialNavigation';
 
 
 export default function App() {
@@ -23,19 +15,6 @@ export default function App() {
 }
 
 
-const Tab = createMaterialBottomTabNavigator();
-
-function MyTabMaterialNavigation() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Academic" component={AcademicScreen} />
-      <Tab.Screen name="Professional" component={ProfessionalScreen} />
-      <Tab.Screen name="Skills" component={SkillsScreen} />   
-      <Tab.Screen name="ExtraActivities" component={ExtraActivitiesScreen} />      
-    </Tab.Navigator>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/MyTabMaterialNavigation.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { HomeScreen } from '../screens/HomeScreen'
+import { AcademicScreen } from '../screens/AcademicScreen'
+import { ProfessionalScreen } from '../screens/ProfessionalScreen'
+import { SkillsScreen } from '../screens/SkillsScreen'
+import { ExtraActivitiesScreen } from '../screens/ExtraActivitiesScreen'
+
+const Tab = createMaterialBottomTabNavigator();
+
+export function MyTabMaterialNavigation() {
+  return (
+    <Tab.Navigator
+      initialRouteName='Home'
+      activeColor='white'
+      barStyle={{ backgroundColor: 'indigo' }}>
+        
+        <Tab.Screen 
+          name='Home'
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Inicio',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }} 
+        />
+        
+        <Tab.Screen
+          name='Academic'
+          component={AcademicScreen}
+          options={{
+            tabBarLabel: 'Acadêmico',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="school" color={color} size={26} />
+            ),
+          }} 
+        />
+        
+        <Tab.Screen
+          name='Professional'
+          component={ProfessionalScreen} 
+          options={{
+            tabBarLabel: 'Professional',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="briefcase" color={color} size={26} />
+            ),
+          }} 
+        />
+        
+        <Tab.Screen
+          name='Skills'
+          component={SkillsScreen}
+          options={{
+            tabBarLabel: 'Competências',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="format-list-bulleted" color={color} size={26} />
+            ),
+          }} 
+        />   
+        
+        <Tab.Screen
+          name='ExtraActivities'
+          component={ExtraActivitiesScreen}
+          options={{
+            tabBarLabel: 'Extras',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="hand-heart" color={color} size={26} />
+            ),
+          }} 
+        />      
+    </Tab.Navigator>
+  );
+}
